bb19cc3990f0873f6e961a9c386f0c06fc841f55
Harsh Agrawal <2003harsh16@gmail.com>
2026-04-02 11:18:23 +0530

    Frontend - Home page designed

---
+++ Frontend/src/data/pricingData.js
@@ -0,0 +1,86 @@
+import { CheckIcon } from "lucide-react";
+
+export const pricingData = [
+    {
+        title: "Basic Plan",
+        price: 5,
+        features: [
+            {
+                name: "20 credits",
+                icon: CheckIcon,
+            },
+            {
+                name: "Standard video quality",
+                icon: CheckIcon,
+            },
+            {
+                name: "No watermark",
+                icon: CheckIcon,
+            },
+            {
+                name: "Slower generation speed",
+                icon: CheckIcon,
+            },
+            {
+                name: "Email support",
+                icon: CheckIcon,
+            },
+        ],
+        buttonText: "Get Started",
+    },
+    {
+        title: "Pro Plan",
+        price: 25,
+        mostPopular: true,
+        features: [
+            {
+                name: "50 credits",
+                icon: CheckIcon,
+            },
+            {
+                name: "HD video export",
+                icon: CheckIcon,
+            },
+            {
+                name: "No watermark",
+                icon: CheckIcon,
+            },
+            {
+                name: "Faster generation",
+                icon: CheckIcon,
+            },
+            {
+                name: "Priority support",
+                icon: CheckIcon,
+            }
+        ],
+        buttonText: "Upgrade Now",
+    },
+    {
+        title: "Enterprise Plan",
+        price: 50,
+        features: [
+            {
+                name: "500 credits",
+                icon: CheckIcon,
+            },
+            {
+                name: "FHD video export",
+                icon: CheckIcon,
+            },
+            {
+                name: "No watermark",
+                icon: CheckIcon,
+            },
+            {
+                name: "Faster generation",
+                icon: CheckIcon,
+            },
+            {
+                name: "Chat + Email support",
+                icon: CheckIcon,
+            }
+        ],
+        buttonText: "Contact Sales",
+    }
+];
